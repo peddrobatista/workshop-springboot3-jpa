a3b8d5018e0fe9420d9564a8e5ce1f414e05f1a3
pedro-batista <peddrobatista2022@gmail.com>
2024-04-28 11:42:10 -0300

    H2 database, test profile, JPA

--- src/main/java/com/peddrobatista/demo/entities/User.java
+++ src/main/java/com/peddrobatista/demo/entities/User.java
@@ -3,8 +3,17 @@ package com.peddrobatista.demo.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;

7aeb1ab76074e456e50f6b5d60143bca94f3c79d
pedro-batista <peddrobatista2022@gmail.com>
2024-05-08 10:08:17 -0300

    OrderStatus enum

--- src/main/java/com/peddrobatista/demo/config/TestConfig.java
+++ src/main/java/com/peddrobatista/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.peddrobatista.demo.entities.Order;
 import com.peddrobatista.demo.entities.User;
+import com.peddrobatista.demo.entities.enums.OrderStatus;
 import com.peddrobatista.demo.repositories.OrderRepository;
 import com.peddrobatista.demo.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATTING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WATTING_PAYMENT, u1); 
 		
 		// Salvando os objetos no banco de dados
 		
